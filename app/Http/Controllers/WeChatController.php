d5041c765b9a326e9262982ee15926c305d8b071
Mr. Douzhe Li <lidouzhe@126.com>
2018-07-19 19:11:59 +0800

    wechat test

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -14,11 +14,11 @@ class WeChatController extends Controller
      */
     public function serve()
     {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+        Log::info('info request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
         $app->server->push(function($message){
-            return "欢迎关注 overtrue！";
+            return "终于等到你，加入无线通信科研小组，探索前沿科学问题，收获知识、成长、友情和欢乐";
         });
 
         return $app->server->serve();
