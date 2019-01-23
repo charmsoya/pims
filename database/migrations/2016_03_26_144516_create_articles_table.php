5f9c913c2c89b515263f4c30cc297fbcad695797
Mr. Douzhe Li <lidouzhe@126.com>
2019-01-24 00:48:47 +0800

    modify database migration file

--- database/migrations/2016_03_26_144516_create_articles_table.php
+++ database/migrations/2016_03_26_144516_create_articles_table.php
@@ -14,42 +14,38 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('article_records', function (Blueprint $table) {
 		$table->increments('id');
-		$table->primary('id');
 		$table->timestamp('published_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 		$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 		$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 		$table->boolean('online');
 		$table->integer('plate_id');
 	});
+	Schema::create('article_locales', function(Blueprint $table) {
+		$table->increments('id');
+		$table->text('name');
+		$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+		$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));	
+	});
 	Schema::create('article_translations', function (Blueprint $table) {
 		$table->increments('id');
-		$table->primary('id');
 		$table->integer('article_id')->unsigned();
-		$table->string('locale_id')->unsigned();	
+		$table->integer('locale_id')->unsigned();	
 		$table->string('title');
 		$table->text('abstract');
 		$table->text('content');
 		$table->integer('n_read')->unsigned();
 		$table->foreign('article_id')->references('id')->on('article_records')->onDelete('cascade');
-		$table->foreign('locale_id')->references('id')->on('article_locales');
+		$table->foreign('locale_id')->references('id')->on('article_locales')->onDelete('cascade');
 	});
 	Schema::create('article_plates', function(Blueprint $table) {
 		$table->increments('id');
-		$table->primary('id');
 		$table->text('name');
 		$table->text('translate_name');
 		$table->boolean('is_leaf');
 		$table->integer('parent_id');
 		$table->integer('level');
 		$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-		$table->timestamp('create_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-	});
-	Schema::create('article_locales', function(Blueprint $table) {
-		$table->increments('id');
-		$table->primary('id');
-		$table->text('name');
-		$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-		$table->timestamp('create_at')->default(DB::raw('CURRENT_TIMESTAMP'));	
+		$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 	});
     }
 
